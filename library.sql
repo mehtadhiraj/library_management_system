d2f182f50f5373b706717c2b1e2e874ef796c7bb
Dhiraj Mehta <mehtadhiraj21@gmail.com>
2018-03-25 00:18:57 +0530

    Library database commited

---
+++ library.sql
@@ -0,0 +1,111 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 24, 2018 at 07:48 PM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `login`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `book`
+--
+
+CREATE TABLE `book` (
+  `author` varchar(25) NOT NULL,
+  `name` varchar(25) NOT NULL,
+  `isbn` int(15) NOT NULL,
+  `bookid` int(15) NOT NULL,
+  `doiss` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `doret` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `book`
+--
+
+INSERT INTO `book` (`author`, `name`, `isbn`, `bookid`, `doiss`, `doret`) VALUES
+('Dhiraj Mehta', 'Secret of coding', 123, 123456, '2018-03-23 18:30:00', '0000-00-00'),
+('SIES', 'Way to success', 456, 456789, '2018-03-24 10:32:04', '0000-00-00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `issue`
+--
+
+CREATE TABLE `issue` (
+  `prn` varchar(30) NOT NULL,
+  `bookid` int(20) NOT NULL,
+  `issuedate` date NOT NULL,
+  `returndate` date NOT NULL DEFAULT '0000-00-00'
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `issue`
+--
+
+INSERT INTO `issue` (`prn`, `bookid`, `issuedate`, `returndate`) VALUES
+('116A3032', 456789, '2018-03-24', '2018-03-31');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `student`
+--
+
+CREATE TABLE `student` (
+  `name` varchar(20) NOT NULL,
+  `prn` varchar(10) NOT NULL,
+  `branch` varchar(5) NOT NULL,
+  `year` varchar(3) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `student`
+--
+
+INSERT INTO `student` (`name`, `prn`, `branch`, `year`) VALUES
+('Siddhesh Menon', '116A3032', 'IT', 'SE'),
+('Abhirup Kamath', '116A3021', 'IT', 'SE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user` varchar(10) NOT NULL,
+  `pass` varchar(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user`, `pass`) VALUES
+('hello', 'world'),
+('test', 'test');
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
