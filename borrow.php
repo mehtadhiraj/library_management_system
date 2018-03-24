a68ec01fb7c1506e9e2a91cb5a8b800dbfc19ebc
Dhiraj Mehta <mehtadhiraj21@gmail.com>
2018-03-24 19:31:51 +0530

    Proper Borrow form commited

--- borrow.php
+++ borrow.php
@@ -1,109 +1,118 @@
+<!DOCTYPE html>
 <html>
 	<head>
 		<link rel="stylesheet" type="text/css" href="css.css">
-		<script type="text/javascript">
-			function check()
-			{
-				if((document.f1.t1.value=="")||(document.form1.t3.value=="")||(document.form1.t4.value=="")||(document.f2.t1.value=="")||(document.f2.t2.value=="")||(document.f2.t3.value=="")||(document.f2.t4.value==""))
-				{
-					alert("Please enter complete details")
-					document.f1.t1.focus()
-				}
-			}
-			
-			function check2()
-			{
-				if(document.f2.t2.value=="")
-				{
-					alert("Please enter PRN number.")
-					document.f2.t2.focus()
-				}
-			}
-		</script>
 	</head>
 	<body>
+		<?php 
+			@$username = "root";
+ 			@$passwd = "";
+ 			@$dbname = "login";
+ 			@$host = "localhost";	
+ 			
+			@$dbconn = mysqli_connect($host, $username, $passwd, $dbname) ;
+		?>
 		<form method="POST">
-		<center>
-		<h2 align="center"> Borrow </h2>
-		<h4 align="center"> Enter Book ID </h4>
-		<input type='text' id="bookid" name='bookid'><br>
-		<input type="submit" class="button button2" value="Fetch" Name="submit">
-	    		  <?php
- 	@$username = "root";
- 	@$passwd = "";
- 	@$dbname = "login";
- 	@$host = "localhost";	
- 	@$id=$_POST['bookid'];
-	@$dbconn = mysqli_connect($host, $username, $passwd, $dbname) ;
-	if(isset($_POST['submit']))
-	{
-		if($id==""){
-			echo "<script>alert('Please enter the BookID')</script>";
-		}else{
- 	$sql1= "select * from book where bookid=$id";
- 	$res1= mysqli_query($dbconn,$sql1);
-
-	while($row = mysqli_fetch_assoc($res1)){
-	echo "		<table cellpadding='10px'>
+			<center>
+				<h2 align="center"> Issue a book </h2>
 				<tr>
-					<td>Book ID: </td>
-					<td><input type='text' id='id' name='id' readonly value= ".$row['bookid']."></td>
+					<td>Book ID</td>
+					<td><input type='text' id="askbookid" name='askbookid'><br></td>
 				</tr>
-
-   				<tr>
-					<td>Book Name: </td>
-					<td><input type='text' id='t1' name='t1' readonly value= ".$row['name']."></td>
-					<td>Author: </td>
-					<td><input type='text' id='t2' name='t2' readonly value=".$row['author']."></td>
-				</tr>
+				<br><br>
 				<tr>
-					<td>ISBN: </td>
-					<td><input type='text' id='t3' name='t3' readonly value=".$row['isbn']."></td>
-				</tr>
-				<tr>
-					<td>Borrow Date: </td>
-					<td>
-						<input type=textbox id = 'date' readonly value=".$row['doiss'].">
-					</td>
-					<td>Return: </td>
-					<td>
-						<input type=textbox  readonly value=".date('d/m/Y', strtotime('+7 day', time())).">
-					</td>
-				</tr>
-			</table>
-		"; }
-		}
-	}
-	 ?> 
-	</center>
-</form>
-			<table cellpadding="5px" cellspacing="5px" align="center">
-
-
-
-
-		<h3 align="center"> Student Details </h3>
-		<center>
-		<form name='f2'>
-			<table cellpadding="5px" cellspacing="5px" align="center">
-				<tr>
-					<td> Name: </td>
-					<td> <input type="text" id="t1" name='t1'> </td>
 					<td> PRN No.: </td>
-					<td> <input type="text" id="t2" name='t2'> </td>
+					<td> <input type="text" id="askPrn" name='askPrn'></td>
 				</tr>
-				<tr>
-					<td> Branch: </td>
-					<td> <input type="text" id="t3" name='t3'> </td>
-					<td> Year: </td>
-					<td> <input type="text" id="t4" name='t4'> </td>
-				</tr>
-			</table>
-				<input type="submit" class="button button2" value="Fetch" id="f2" Name="Fetch2" onclick="check2()">
-		</form>
-		<input align="center" type="submit" class="button button2" value="Issue" Name="Issue" onclick="check()">
-		</center>
+				<br><br><br>
+				<input type="submit" class="button button2" value="Fetch" id="f2" Name="Fetch2">
+				<?php
+					@$prn=$_POST['askPrn'];
+					@$id=$_POST['askbookid'];
+					if(isset($_POST['Fetch2'])){
+						$sql = "select * from student where prn='$prn'";
+						$res = mysqli_query($dbconn,$sql);
+						$sql1= "select * from book where bookid=$id";
+ 						$res1= mysqli_query($dbconn,$sql1);
 
+						if($prn == "" || $id == ""){
+							echo "<script>alert('Please enter the values')</script>";
+						}else{
+							while ($row = mysqli_fetch_assoc($res)){
+								echo "<br><br><br>
+									  <table cellpadding=10px>
+										<tr>
+											<td><h4>Student Details</h4></td>
+										</tr>
+										<tr>
+											<td>PRN</td>
+											<td><input type=text readonly name=prn id=prn value=".$row['prn']."></td>
+											<td>Name</td>
+											<td><input type=text readonly name=studentName id=studentName value=".$row['name']."></td>
+										</tr>
+	
+										<tr>
+											<td>Branch</td>
+											<td><input type=text readonly name=branch id=branch value=".$row['branch']."></td>
+											<td>Year</td>
+											<td><input type=text readonly name=year id=year value=".$row['year']."></td>
+										</tr>
+										<tr>
+											<td><h4>Book Details</h4></td>
+										</tr>";
+							}
+							while ($row = mysqli_fetch_assoc($res1)){
+								echo "		<tr>
+												<td>Book ID: </td>
+												<td><input type='text' id='bookId' name='bookId' readonly value= ".$row['bookid']."></td>
+											</tr>
+
+   											<tr>
+												<td>Book Name: </td>
+												<td><input type='text' id='bookName' name='bookName' readonly value= ".$row['name']."></td>
+												<td>Author: </td>
+												<td><input type='text' id='bbokAuthor' name='bookAuthor' readonly value=".$row['author']."></td>
+											</tr>
+										
+											<tr>
+												<td>ISBN: </td>
+												<td><input type='text' id='isbn' name='isbn' readonly value=".$row['isbn']."></td>
+											</tr>
+										
+											<!--<tr>
+												<td>Borrow Date: </td>
+												<td><input type=textbox id = 'date' readonly value=".date('d/m/Y')."></td>
+												<td>Return: </td>
+												<td><input type=textbox  readonly value=".date('d/m/Y', strtotime('+7 day', time()))."></td>
+											</tr>-->
+										</table>
+										<br>
+			 							<br>
+			 							<br>"; 
+							}
+						}
+						echo "<input type=submit name=Issue id=Issue value=Issue class='button button2'>";
+					}
+			 	
+
+					if (isset($_POST['Issue'])) {
+						$prn = $_POST['prn'];
+						$book = $_POST['bookId'];
+						$returndate = date('Y-m-d', strtotime('+7 day', time()));
+						$sql = "select prn from issue where prn='$prn'";
+						$res = mysqli_query($dbconn,$sql);
+						$count = mysqli_num_rows($res);
+						if($count>=3){
+							echo "<script>alert('CANNOT ISSUE MORE THAN THREE BOOKS')</script>";
+						}else{
+							$sqlInsert = "INSERT INTO issue (prn, bookid, issuedate, returndate) VALUES ('$prn', $book, Curdate(), '$returndate')";
+							$res = mysqli_query($dbconn,$sqlInsert);
+							echo "<script>alert('Book issued successfully')</script>";
+						}
+					}
+				?>
+		</center>
+		</form>
 
 	</body>
 </html>
